719cf1f8c415da9e3e8873c7f0c547b52fa41da5
mizutaku-0519 <mizutaku.biz@gmail.com>
2024-11-09 19:45:47 +0900

    Bootstrapのデザインをあてました

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 const app = express();
@@ -17,8 +18,10 @@ mongoose
     console.log(err);
   });
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
